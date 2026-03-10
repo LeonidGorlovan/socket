0d7eed3381327be172c8565902f6884668e65be3
Leonid Gorlovan <dev.leoni.gor@gmail.com>
2026-03-10 21:40:42 +0200

    rename message routes to chat, update sidebar component, and add chat blade

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -1,14 +1,12 @@
 <flux:sidebar.nav>
-    <flux:sidebar.group :heading="__('Platform')" class="grid">
-        <flux:sidebar.item icon="home" :href="route('dashboard')" :current="request()->routeIs('dashboard')" wire:navigate>
-            {{ __('Dashboard') }}
-        </flux:sidebar.item>
-    </flux:sidebar.group>
+    <flux:sidebar.item icon="home" :href="route('dashboard')" :current="request()->routeIs('dashboard')" wire:navigate>
+        {{ __('Dashboard') }}
+    </flux:sidebar.item>
 
-    <flux:sidebar.group :heading="__('Messages')" class="grid">
+    <flux:sidebar.group icon="chat-bubble-bottom-center-text" expandable :heading="__('Chat room with:')" class="grid">
         @if(!empty($users) && count($users))
             @foreach($users as $user)
-                <flux:sidebar.item icon="home" :href="route('message.recipient', ['id' => $user->id])" :current="request()->routeIs('message.recipient') && (int) request()->route('id') === (int) $user->id" wire:navigate>
+                <flux:sidebar.item :href="route('chat.recipient', ['id' => $user->id])" :current="request()->routeIs('chat.recipient') && (int) request()->route('id') === (int) $user->id" wire:navigate>
                     {{ $user->name }}
                 </flux:sidebar.item>
             @endforeach
--- routes/web.php
+++ routes/web.php
@@ -7,8 +7,8 @@ Route::view('/', 'welcome')->name('home');
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::view('dashboard', 'dashboard')->name('dashboard');
 
-    Route::group(['prefix' => 'message'], function () {
-        Route::view('message/recipient/{id}', 'message')->name('message.recipient');
+    Route::group(['prefix' => 'chat', 'as' => 'chat.'], function () {
+        Route::view('recipient/{id}', 'chat')->name('recipient');
     });
 });
 
